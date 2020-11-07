384fd4f2a63fddde45a57a4be8a1f692a3629d20
A1nu <ainu.develop@gmail.com>
2020-11-07 18:06:10 +0200

    Updating docs generation

--- src/main/java/ee/a1nu/application/web/controller/DocumentationController.java
+++ src/main/java/ee/a1nu/application/web/controller/DocumentationController.java
@@ -1,11 +1,10 @@
 package ee.a1nu.application.web.controller;
 
-import ee.a1nu.application.commandConstants.CommandName;
+import ee.a1nu.application.commandConstants.CommandNamesService;
 import ee.a1nu.application.commandConstants.CurrencyCommandNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -13,10 +12,18 @@ import java.util.List;
 @Controller
 public class DocumentationController {
 
+    private final CommandNamesService commandNamesService;
+
+    @Autowired
+    public DocumentationController(CommandNamesService commandNamesService) {
+        this.commandNamesService = commandNamesService;
+    }
+
     @GetMapping(value="/documentation")
     public ModelAndView documentation() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("CommandNames",CurrencyCommandNames.getCommandNames());
+        modelAndView.addObject("commandCategories", commandNamesService.getAllCategoriesWithCommand());
         modelAndView.setViewName("view/documentation");
         return modelAndView;
     }
